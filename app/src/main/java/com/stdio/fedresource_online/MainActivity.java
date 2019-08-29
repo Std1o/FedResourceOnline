0c94690176c386005a61c7a3c56f5150332ca83c
Stdio <kwork-stdio@mail.ru>
2019-08-29 21:59:31 +0300

    0.0.1

--- app/src/main/java/com/stdio/fedresource_online/MainActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/MainActivity.java
@@ -3,12 +3,38 @@ package com.stdio.fedresource_online;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox checkBoxAgreement;
+    MaterialButton materialButtonContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        materialButtonContinue.setEnabled(false);
+    }
+
+    private void initViews() {
+        checkBoxAgreement = findViewById(R.id.checkBoxAgreement);
+        materialButtonContinue = findViewById(R.id.btn_continue);
+
+        checkBoxAgreement.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (checkBoxAgreement.isChecked()) {
+                    materialButtonContinue.setEnabled(true);
+                }
+                else {
+                    materialButtonContinue.setEnabled(false);
+                }
+            }
+        });
     }
 }
