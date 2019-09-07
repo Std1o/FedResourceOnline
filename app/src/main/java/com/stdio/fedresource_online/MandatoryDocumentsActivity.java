ab3850dad1f15237049a861cc6466bb0fc2b1782
Stdio <kwork-stdio@mail.ru>
2019-09-07 10:27:56 +0300

    0.0.5

--- app/src/main/java/com/stdio/fedresource_online/MandatoryDocumentsActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/MandatoryDocumentsActivity.java
@@ -2,7 +2,9 @@ package com.stdio.fedresource_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MandatoryDocumentsActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MandatoryDocumentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mandatory_documents);
+    }
+
+    public void toFourthActivity(View view) {
+        startActivity(new Intent(this, UploadPassportPhotoActivity.class));
     }
 }
---
+++ app/src/main/java/com/stdio/fedresource_online/UploadPassportPhotoActivity.java
@@ -0,0 +1,14 @@
+package com.stdio.fedresource_online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadPassportPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_passport_photo);
+    }
+}
