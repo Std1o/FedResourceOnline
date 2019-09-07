d25be1e6c44d2a19e2c7cfc4682f9eca6f79ceec
Stdio <kwork-stdio@mail.ru>
2019-09-07 20:49:21 +0300

    0.1.0

--- app/src/main/java/com/stdio/fedresource_online/FeedBackInputActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/FeedBackInputActivity.java
@@ -2,7 +2,9 @@ package com.stdio.fedresource_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FeedBackInputActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class FeedBackInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back_input);
+    }
+
+    public void toCompleteActivity(View view) {
+        startActivity(new Intent(this, CompleteActivity.class));
     }
 }
