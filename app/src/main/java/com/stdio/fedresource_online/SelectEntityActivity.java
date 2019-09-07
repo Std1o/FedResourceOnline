dab1611813c0c0ad9d99cea9f4fa2b32c2d22684
Stdio <kwork-stdio@mail.ru>
2019-09-07 22:14:00 +0300

    0.1.3

--- app/src/main/java/com/stdio/fedresource_online/SelectEntityActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/SelectEntityActivity.java
@@ -8,13 +8,27 @@ import android.view.View;
 
 public class SelectEntityActivity extends AppCompatActivity {
 
+    public static String entity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_entity);
     }
 
-    public void toThirdActivity(View view) {
+    public void toMandatoryDocumentsActivity(View view) {
+        switch (view.getId()) {
+            case R.id.btn_legal_entity:
+                entity = getResources().getString(R.string.legal_entity);
+                break;
+            case R.id.btn_individual_entrepreneur:
+                entity = getResources().getString(R.string.individual_entrepreneur);
+                break;
+            case R.id.btn_natural_person:
+                entity = getResources().getString(R.string.natural_person);
+                break;
+        }
+        System.out.println(entity);
         startActivity(new Intent(this, MandatoryDocumentsActivity.class));
     }
 }
