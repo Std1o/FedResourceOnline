bcc56b3c6dd379cae5bab3cb6af46f9727396062
Stdio <kwork-stdio@mail.ru>
2019-09-07 16:33:38 +0300

    0.0.6

--- app/src/main/java/com/stdio/fedresource_online/UploadPassportPhotoActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/UploadPassportPhotoActivity.java
@@ -2,13 +2,63 @@ package com.stdio.fedresource_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.button.MaterialButton;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
 
 public class UploadPassportPhotoActivity extends AppCompatActivity {
+
+    ImageView ivPassportPhoto;
+    Bitmap passportPhoto;
+    MaterialButton btnSelectImage, btnDeleteImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_passport_photo);
+        init();
+        btnDeleteImage.setEnabled(false);
+    }
+
+    private void init() {
+        ivPassportPhoto = findViewById(R.id.ivPassportPhoto);
+        btnSelectImage = findViewById(R.id.btnSelect);
+        btnDeleteImage = findViewById(R.id.btnDelete);
+    }
+
+    public void selectImage(View view) {
+        Intent intent = new Intent(this, ImageSelectActivity.class);
+        intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_GALLERY, true);//default is true
+        startActivityForResult(intent, 1213);
+    }
+
+    public void deleteImage(View view) {
+        passportPhoto = null;
+        ivPassportPhoto.setImageDrawable(getResources().getDrawable(R.drawable.passport_example));
+        btnDeleteImage.setEnabled(false);
+        btnSelectImage.setEnabled(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1213 && resultCode == Activity.RESULT_OK) {
+            String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+            Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
+            passportPhoto = selectedImage;
+            ivPassportPhoto.setImageBitmap(selectedImage);
+            btnDeleteImage.setEnabled(true);
+            btnSelectImage.setEnabled(false);
+        }
     }
 }
