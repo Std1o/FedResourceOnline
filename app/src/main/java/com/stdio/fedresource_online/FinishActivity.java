2f5a9fc7cafd262f8c6f6d351bc120e506d39264
Stdio <kwork-stdio@mail.ru>
2019-09-07 21:55:47 +0300

    0.1.2

--- app/src/main/java/com/stdio/fedresource_online/FinishActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/FinishActivity.java
@@ -2,10 +2,14 @@ package com.stdio.fedresource_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 public class FinishActivity extends AppCompatActivity {
+
+    String phone = "tel:88005553535";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,11 @@ public class FinishActivity extends AppCompatActivity {
 
     public void finishActivity(View view) {
         finish();
+    }
+
+    public void callIntent(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL,
+                Uri.parse(phone));
+        startActivity(intent);
     }
 }
