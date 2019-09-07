b0d6fab5f068697f73872b71f0ec3fc2a771a12d
Stdio <kwork-stdio@mail.ru>
2019-09-07 23:01:56 +0300

    0.1.5

--- app/src/main/java/com/stdio/fedresource_online/FeedBackInputActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/FeedBackInputActivity.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class FeedBackInputActivity extends AppCompatActivity {
+
+    public static String phone, mail;
+    EditText etPhone, etMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back_input);
+        init();
+    }
+
+    private void init() {
+        etPhone = findViewById(R.id.etPhone);
+        etMail = findViewById(R.id.etMail);
     }
 
     public void toCompleteActivity(View view) {
+        phone = etPhone.getText().toString();
+        mail = etMail.getText().toString();
+        System.out.println(phone + "\n" + mail);
         startActivity(new Intent(this, CompleteActivity.class));
     }
 }
