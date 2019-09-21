2c3697cdfe6d31b140a7251ad4971f8032a0c5cf
Stdio <kwork-stdio@mail.ru>
2019-09-21 20:29:01 +0300

    0.2.5

--- app/src/main/java/com/stdio/fedresource_online/WebviewActivity.java
+++ app/src/main/java/com/stdio/fedresource_online/WebviewActivity.java
@@ -2,27 +2,35 @@ package com.stdio.fedresource_online;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WebviewActivity extends AppCompatActivity {
 
     private WebView webView;
     public static String url;
+    public static ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+        progressBar = findViewById(R.id.progressBar);
 
         webView = findViewById(R.id.webView);
         // включаем поддержку JavaScript
         webView.getSettings().setJavaScriptEnabled(true);
         // указываем страницу загрузки
         webView.loadUrl(url);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new MyWebViewClient());
     }
 
     @Override
@@ -31,6 +39,30 @@ public class WebviewActivity extends AppCompatActivity {
             webView.goBack();
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public class MyWebViewClient extends WebViewClient {
+
+        public MyWebViewClient() {
+            super();
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Log.d("urll", "URL地址:" + url);
+            progressBar.setVisibility(View.VISIBLE);
+            webView.setVisibility(View.GONE);
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Log.i("urll", "onPageFinished");
+            CookieSyncManager.getInstance().sync();
+            progressBar.setVisibility(View.GONE);
+            webView.setVisibility(View.VISIBLE);
+            super.onPageFinished(view, url);
         }
     }
 }
